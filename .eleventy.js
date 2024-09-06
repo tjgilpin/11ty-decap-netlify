38d3a477095eeb1607d6927f6633d34e490b860c
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2024-09-06 22:10:03 +0100

    Remove alpinejs for real life

--- .eleventy.js
+++ .eleventy.js
@@ -27,7 +27,6 @@ module.exports = function (eleventyConfig) {
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
     "./src/admin/config.yml": "./admin/config.yml",
-    "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
     "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
   });
