9d83279df92c2c9c84ae992137b366c752c374db
Tim Gilpin <1889642+tjgilpin@users.noreply.github.com>
2024-10-10 12:53:38 +0100

    Remove redirect

--- .eleventy.js
+++ .eleventy.js
@@ -40,7 +40,6 @@ module.exports = function (eleventyConfig) {
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
   eleventyConfig.addPassthroughCopy("./src/static/css");
-  eleventyConfig.addPassthroughCopy("./src/_redirects");
 
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
